57b55787dd8c2d8838a5061558fe96c89f481c50
Belous <belous@iMac-Belous.local>
2019-10-11 23:17:04 +0300

    update

--- src/app/common/factories/http-loader.factory.ts
+++ src/app/common/factories/http-loader.factory.ts
@@ -2,5 +2,5 @@ import { HttpClient } from '@angular/common/http';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 export function HttpLoaderFactory(httpClient: HttpClient) {
-  return new TranslateHttpLoader(httpClient, 'assets/i18n/', '.json');
+	return new TranslateHttpLoader(httpClient, 'assets/i18n/', '.json');
 }
