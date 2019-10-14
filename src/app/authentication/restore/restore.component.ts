8d4581a4dcc61972c0a18b74b92aac4b168e8d6e
Владимир Белоус <v.belous@ktru.info>
2019-10-14 21:06:27 +0300

    temprorary save

--- src/app/authentication/restore/restore.component.ts
+++ src/app/authentication/restore/restore.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { HostBinding } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormControlService } from '@air-services/form-control.service';
+import { OverlayContainer } from '@angular/cdk/overlay';
+import { VisitorFormArray, FormControlActions } from '@air-models/visitor';
 
 @Component({
 	selector: 'app-restore',
@@ -7,9 +12,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RestoreComponent implements OnInit {
 
-	constructor() { }
+	@HostBinding('class') componentCssClass;
+	public loginForm: FormGroup;
 
-	ngOnInit() {
+	constructor(
+		private formBuilder: FormBuilder,
+		private formControlService: FormControlService,
+		private overlayContainer: OverlayContainer,
+	) {
+
 	}
 
+	ngOnInit() {
+		this.overlayContainer.getContainerElement().classList.add('authentication');
+		this.componentCssClass = 'authentication';
+		const formControlActions: VisitorFormArray = this.formControlService.getFormArrayByAction(FormControlActions.Restore);
+		this.loginForm = this.formBuilder.group({
+			ownControls: this.formBuilder.array([]),
+		});
+		const ownControls = this.loginForm.get('ownControls') as VisitorFormArray;
+		formControlActions.controls.forEach( fc => {
+			ownControls.push(fc);
+		});
+	}
+	get ownControls() {
+		return this.loginForm.get('ownControls') as VisitorFormArray;
+	}
+
+	onSubmit() {
+
+	}
 }
--- src/app/common/models/visitor/form-controls.dto.ts
+++ src/app/common/models/visitor/form-controls.dto.ts
@@ -1,5 +1,6 @@
 export enum FormControlActions {
 	Login = 'Вход в учетную запись',
+	Restore = 'Восстановление пароля',
 }
 
 export enum FormControlFields {
